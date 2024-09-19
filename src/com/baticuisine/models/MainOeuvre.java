b1dee84d304e88d93f5c39fa8730ca7909789c93
asmaabarj <asmaabarj5@gmail.com>
2024-09-19 14:54:47 +0100

    mainOeuvre model

--- src/com/baticuisine/models/MainOeuvre.java
+++ src/com/baticuisine/models/MainOeuvre.java
@@ -1,4 +1,40 @@
 package com.baticuisine.models;
 
-public class MainOeuvre {
+public class MainOeuvre extends Composant {
+    private double tauxHoraire;
+    private double heuresTravail;
+    private double productiviteOuvrier;
+
+    public MainOeuvre(int id, String nom, double coutUnitaire, double quantite, double tauxTVA, double coutTransport, double coefficientQualite,
+                      double tauxHoraire, double heuresTravail, double productiviteOuvrier) {
+        super(id, nom, coutUnitaire, quantite, tauxTVA, coutTransport, coefficientQualite);
+        this.tauxHoraire = tauxHoraire;
+        this.heuresTravail = heuresTravail;
+        this.productiviteOuvrier = productiviteOuvrier;
+    }
+
+    // Getters et setters
+    public double getTauxHoraire() {
+        return tauxHoraire;
+    }
+
+    public void setTauxHoraire(double tauxHoraire) {
+        this.tauxHoraire = tauxHoraire;
+    }
+
+    public double getHeuresTravail() {
+        return heuresTravail;
+    }
+
+    public void setHeuresTravail(double heuresTravail) {
+        this.heuresTravail = heuresTravail;
+    }
+
+    public double getProductiviteOuvrier() {
+        return productiviteOuvrier;
+    }
+
+    public void setProductiviteOuvrier(double productiviteOuvrier) {
+        this.productiviteOuvrier = productiviteOuvrier;
+    }
 }
