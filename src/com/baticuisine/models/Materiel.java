f7a78b50a11ad11c3dcaa8ab61b098028859a394
asmaabarj <asmaabarj5@gmail.com>
2024-09-19 14:55:44 +0100

    materiel model

--- src/com/baticuisine/models/Materiel.java
+++ src/com/baticuisine/models/Materiel.java
@@ -1,4 +1,29 @@
 package com.baticuisine.models;
 
-public class Materiel {
+public class Materiel extends Composant {
+    private double quantite;
+    private double coutUnitaire;
+
+    public Materiel(int id, String nom, double coutUnitaire, double quantite, double tauxTVA, double coutTransport, double coefficientQualite) {
+        super(id, nom, coutUnitaire, quantite, tauxTVA, coutTransport, coefficientQualite);
+        this.quantite = quantite;
+        this.coutUnitaire = coutUnitaire;
+    }
+
+    // Getters et setters
+    public double getQuantite() {
+        return quantite;
+    }
+
+    public void setQuantite(double quantite) {
+        this.quantite = quantite;
+    }
+
+    public double getCoutUnitaire() {
+        return coutUnitaire;
+    }
+
+    public void setCoutUnitaire(double coutUnitaire) {
+        this.coutUnitaire = coutUnitaire;
+    }
 }
